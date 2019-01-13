ce248c7707967a02023b2303ce7a86089344691a
Chidi Orji <orjichidi95@gmail.com>
2019-01-13 20:51:03 +0100

    feat: Mount books on load

--- src/App.js
+++ src/App.js
@@ -4,9 +4,34 @@ import './App.css'
 
 class BooksApp extends React.Component {
 
+    state = {
+        books: [],
+    }
+
+    componentDidMount() {
+        BooksAPI.getAll()
+        .then((books) => {
+            this.setState({books: books})
+        });
+    }
+
     render() {
+
+        console.log(JSON.stringify(this.state))
+
+        const shelves = ['Currently Reading', 'Want to Read', 'Read']
         return (
-          <div></div>
+          <div>
+              <div className='list-books-title'>
+                <h1>My Reads - Chidi Orji</h1>
+              </div>
+
+                {
+                    shelves.map((shelf) => (
+                        <h2 key={shelf}>{shelf}</h2> // render shelf here
+                    ))
+                }
+          </div>
         )
     }
 }
