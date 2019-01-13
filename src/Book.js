0482a0b0fc4c64550ceda0d61f27ee26e9d6d4d3
Chidi Orji <orjichidi95@gmail.com>
2019-01-13 22:45:27 +0100

    feat: Update book shelf by select dropdown

--- src/Book.js
+++ src/Book.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import * as BooksAPI from './BooksAPI';
+
 class Book extends Component {
 
-    state = {
-        status: ""
-    };
+    moveBook = (shelf) => {
+        BooksAPI.update(this.props.book, shelf)
+    }
 
     render() {
 
@@ -16,23 +18,24 @@ class Book extends Component {
                     <div className="book-top">
                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                         <div className="book-shelf-changer">
-                            <select>
+                            <select onChange={(e) => this.moveBook(e.target.value)}>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
                                 <option value="none">None</option>
                             </select>
-                    </div>ct>
+                    </div>
                     </div>
                     <div className="book-title">{book.title}</div>
 
                     {
                         book.authors.map((author) => (
-                            <div className="book-authors">{author}</div>
+                            <div key={author} className="book-authors">{author}</div>
                         ))
                     }
                 </div>
+                <p>Temporary variable: {book.shelf}</p>
             </li>
         );
     };
