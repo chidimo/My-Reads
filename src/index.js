8879b9e56704260a57253f725124f6a1126e33f7
Chidi Orji <orjichidi95@gmail.com>
2019-01-14 01:08:15 +0100

    feat: Add router to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, document.getElementById('root'))
