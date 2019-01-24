3f616f125f2716cb14fbafa0840a396cfba179e8
Chidi Orji <orjichidi95@gmail.com>
2019-01-24 03:25:13 +0100

    refactor: Change to function component

--- src/Book.js
+++ src/Book.js
@@ -1,38 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 
-class Book extends Component {
-
-    render() {
-
-        const { book, book_mover } = this.props
-
-        return (
-            <li>
-                <div className="book">
-                    <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                        <div className="book-shelf-changer">
-                            <select onChange={(e) => book_mover(e.target.value, book)}>
-                                <option defaultValue value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
-                    </div>
-                    </div>
-                    <div className="book-title">{book.title}</div>
-
-                    {
-                        book.authors.map((author) => (
-                            <div key={author} className="book-authors">{author}</div>
-                        ))
-                    }
+const Book = ({book, book_mover}) => {
+    return (
+        <li>
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                    <div className="book-shelf-changer">
+                        <select onChange={(e) => book_mover(e.target.value, book)}>
+                            <option defaultValue value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
                 </div>
-            </li>
-        );
-    };
+                </div>
+                <div className="book-title">
+                    {book.title}
+                </div>
+    
+                {
+                    book.authors.map((author) => (
+                        <div key={author} className="book-authors">{author}</div>
+                    ))
+                }
+            </div>
+        </li>
+    );
 }
 
 export default Book
