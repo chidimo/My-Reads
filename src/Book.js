a747fb91d390951213e94e6f8b599cc69234e896
orji chidi <orjichidi95@gmail.com>
2019-01-25 07:29:38 +0100

    fix: Set current shelf on book shelf dropdown

--- src/Book.js
+++ src/Book.js
@@ -8,7 +8,7 @@ const Book = ({book, book_mover}) => {
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                     <div className="book-shelf-changer">
-                        <select onChange={(e) => book_mover(e.target.value, book)}>
+                        <select value={book.shelf}  onChange={(e) => book_mover(e.target.value, book)}>
                             <option defaultValue value="move" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
