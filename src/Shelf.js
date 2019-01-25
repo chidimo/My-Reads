d635d19a3cd1d46830ce0a8ff2ce05425cd9434c
Chidi Orji <orjichidi95@gmail.com>
2019-01-25 15:13:42 +0100

    refactor: Make it a function component

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,42 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Book from './Book.js';
 
-class Shelf extends Component {
+const Shelf = ({ shelf_code_name, books, book_mover }) => {
 
-    render() {
-
-        const shelf_codes = {
-            currentlyReading : 'Currently Reading',
-            wantToRead : 'Want to read',
-            read : 'Read'
-        }
-        const {shelf_code_name, books, book_mover } = this.props
-
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{shelf_codes[shelf_code_name]}</h2>
-
-               <div className="bookshelf-books">
-                    <ol className="books-grid">
-        
-                        {
-                            books.filter((book) => (
-                                book.shelf === shelf_code_name
-                            ))
-                            .map((book) => (
-                                <Book
-                                    key={book.id}
-                                    book={book}
-                                    book_mover={book_mover}
-                                />
-                            ))
-                        }
-                    </ol>
-               </div>
-            </div>
-        )
+    const shelf_codes = {
+        currentlyReading : 'Currently Reading',
+        wantToRead : 'Want to read',
+        read : 'Read'
     }
+    
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{shelf_codes[shelf_code_name]}</h2>
+
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+    
+                    {
+                        books.filter((book) => (
+                            book.shelf === shelf_code_name
+                        ))
+                        .map((book) => (
+                            <Book
+                                key={book.id}
+                                book={book}
+                                book_mover={book_mover}
+                            />
+                        ))
+                    }
+                </ol>
+            </div>
+        </div>
+    )
 }
 
 export default Shelf
