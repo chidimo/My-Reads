3801ecbc88a33c4bee02136dde4f317f1fa5049b
Chidi Orji <orjichidi95@gmail.com>
2019-01-25 17:51:45 +0100

    feat: Use switch to render routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Switch>
+            <App />
+        </Switch>
     </BrowserRouter>, document.getElementById('root'))
